cb98b7b79022450134de248bc358355a2de2891e
Yumin Song <yms1204@snu.ac.kr>
2025-06-19 17:21:26 +0900

    Fix font and asset prefix

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/umin/Desktop/Yumin/Homepage/yms1204.github.io/app/layout":["static/media/e5a4b6cc4d9f838f-s.p.woff2","static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{},"appUsingSizeAdjust":false,"pagesUsingSizeAdjust":false}';
